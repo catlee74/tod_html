e86850f1abb0cafb4fca2bf84104e0d951f029bf
catlee74 <162817725+catlee74@users.noreply.github.com>
2025-11-11 22:21:36 +0900

    마우스 커서 수정

--- mouse_cursor.js
+++ mouse_cursor.js
@@ -1,6 +1,5 @@
 let mouseCursor = document.querySelector('.mouse_cursor');
-let navLinks = document.querySelectorAll('.gnb li a'); //메뉴 링크
-//window 객체에 scroll & mouse 이벤트를 추가하고 cursor함수 실행되도록 함
+
 window.addEventListener('scroll', cursor);
 window.addEventListener('mousemove', cursor);
 //커스텀 커서의 left값과 top값을 커서의 XY좌표값과 일치시킴
@@ -8,3 +7,30 @@ function cursor(e) {
   mouseCursor.style.left = e.pageX + 'px';
   mouseCursor.style.top = e.pageY - scrollY + 'px';
 }
+
+// let mouseCursor = document.querySelector('.mouse_cursor');
+
+// // 마우스좌표
+// let mouseX = 0;
+// let mouseY = 0;
+
+// // 커서좌표
+// let cursorX = 0;
+// let cursorY = 0;
+
+// window.addEventListener('mousemove', (e) => {
+//   mouseX = e.pageX;
+//   mouseY = e.pageY;
+// });
+
+// function animateCursor() {
+//   cursorX += (mouseX - cursorX) * 0.15;
+//   cursorY += (mouseY - cursorY) * 0.15;
+
+//   mouseCursor.style.left = cursorX + 'px';
+//   mouseCursor.style.top = cursorY + 'px';
+
+//   requestAnimationFrame(animateCursor);
+// }
+
+// animateCursor();
